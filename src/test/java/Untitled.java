d3a9898059b0ad4c749b05b1e7ff4564568ed053
mor.hazan <mor.hazan@experitest.com>
2018-07-12 14:33:22 +0300

    Final

--- src/test/java/Untitled.java
+++ src/test/java/Untitled.java
@@ -16,38 +16,40 @@ import java.util.logging.Level;
 public class Untitled {
     private String reportDirectory = "reports";
     private String reportFormat = "xml";
-    private String testName = "Jenkins Android";
-    private String accessKey = "eyJ4cC51IjoxMzY1NDgsInhwLnAiOjIsInhwLm0iOiJNQSIsImFsZyI6IkhTMjU2In0.eyJleHAiOjE4NDI1MTQzOTYsImlzcyI6ImNvbS5leHBlcml0ZXN0In0.zGcYeXkTAcXyNohf9hespTor7r_U9BNYZL0yNbW7Gbg";
+    private String testName = "Untitled";
+    private String accessKey = "eyJ4cC51IjoxMzY1NDgsInhwLnAiOjIsInhwLm0iOiJNQSIsImFsZyI6IkhTMjU2In0.eyJleHAiOjE4NDY1ODg1NTQsImlzcyI6ImNvbS5leHBlcml0ZXN0In0.yJrbvQhXxRMu5CzZOjLw2hUXqyDejrQkO-lC7WROGKw";
     protected AndroidDriver<AndroidElement> driver = null;
 
     DesiredCapabilities dc = new DesiredCapabilities();
-    @Parameters
+
     @BeforeMethod
     public void setUp() throws MalformedURLException {
         dc.setCapability("reportDirectory", reportDirectory);
         dc.setCapability("reportFormat", reportFormat);
         dc.setCapability("testName", testName);
         dc.setCapability("accessKey", accessKey);
-//        dc.setCapability(MobileCapabilityType.UDID, "00db17d862b2bd18");
-        dc.setCapability(MobileCapabilityType.APP, "cloud:com.experitest.eribank/com.experitest.ExperiBank.LoginActivity");
-        dc.setCapability("deviceQuery","@os='android'");
-        dc.setCapability("appVersion", "1.2363");
-        dc.setCapability(AndroidMobileCapabilityType.APP_PACKAGE, "com.experitest.eribank");
-        dc.setCapability(AndroidMobileCapabilityType.APP_ACTIVITY, "com.experitest.ExperiBank.LoginActivity");
+        dc.setCapability("deviceQuery", "@os='android'");
+        dc.setCapability(AndroidMobileCapabilityType.APP_PACKAGE, "com.experitest.ExperiBank");
+        dc.setCapability(AndroidMobileCapabilityType.APP_ACTIVITY, ".LoginActivity");
         dc.setCapability("instrumentApp", true);
         driver = new AndroidDriver<>(new URL("https://sales.experitest.com:443/wd/hub"), dc);
-        System.out.println(driver.getCapabilities().getCapability("reportUrl"));
         driver.setLogLevel(Level.INFO);
     }
 
     @Test
     public void testUntitled() {
+        driver.findElement(By.xpath("//*[@hint='Username']")).click();
+        new WebDriverWait(driver, 60).until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[@hint='Username']")));
+        driver.findElement(By.xpath("//*[@hint='Username']")).click();
+        new WebDriverWait(driver, 120).until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[@hint='Username']")));
         driver.findElement(By.xpath("//*[@hint='Username']")).sendKeys("company");
+        new WebDriverWait(driver, 10).until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[@hint='Password']")));
         driver.findElement(By.xpath("//*[@hint='Password']")).sendKeys("company");
-
-        new WebDriverWait(driver, 10).until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[@text='Login']")));
         driver.findElement(By.xpath("//*[@text='Login']")).click();
-        new WebDriverWait(driver, 10).until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[@text='Logout']")));
+        driver.findElement(By.xpath("//*[@text='Make Payment']")).click();
+        driver.findElement(By.xpath("//*[@text='Cancel']")).click();
+        driver.findElement(By.xpath("//*[@text='Expense Report']")).click();
+        driver.findElement(By.xpath("//*[@text='Back']")).click();
         driver.findElement(By.xpath("//*[@text='Logout']")).click();
         driver.pressKeyCode(AndroidKeyCode.HOME);
     }
